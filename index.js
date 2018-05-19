d997cd52ebf59723d0434922638a6e2cff630b93
Peter van Dijk <peter@petervandijk.net>
2018-05-19 19:41:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
-    return `Currently serving ${katzDeliLine[0]}.`;
+    var retStr = `Currently serving ${katzDeliLine[0]}.`;
     katzDeliLine.shift();
+    return retStr;
   }
 }
