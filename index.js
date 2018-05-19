4839cb525f9cf32746d4b6b6a0d7b44af95f1d75
Peter van Dijk <peter@petervandijk.net>
2018-05-19 19:54:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,10 @@ function currentLine(katzDeliLine) {
     
     for (var i = 0; i < katzDeliLine.length; i++) {
       var linePlace = i + 1;
-      retStr += `${linePlace}. ${katzDeliLine[i]}, `;
+      retStr += `${linePlace}. ${katzDeliLine[i]} `;
+      if (i !== katzDeliLine.length - 1) {
+        retStr += ", ";
+      }
     }
   }
   return retStr;
